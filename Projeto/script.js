d164c4bb9e299a711121a0baa0df48cf6df5ba46
CosmoBatista <cosmobatista911@gmail.com>
2024-02-07 11:45:12 -0300

    :tada initial commit

---
+++ Projeto/script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+    const html= document.documentElement
+    // ir na lista de clase do objeto html
+    // se tiver Light remove se não adicionar
+    html.classList.toggle('Light')
+
+   //if(html.classList.contains('Light')){
+   //     html.classList.remove('Light')
+   // }else{
+   //      html.classList.add('Light')
+   //  }
+
+
+        // ir no objeto img do header 
+    const img = document.querySelector("header img")
+      
+        // se na lista de clase html conter Light
+        if(html.classList.contains("Light")){
+        //seleciona o atributo src de img e adicionar avatar light
+            img.setAttribute("src", "../assets/avatar-light.png" )
+        }else{
+        // se não tiver light, não alterar imagem
+            img.setAttribute("src", "../assets/avatar.png")
+        }
+
+}
